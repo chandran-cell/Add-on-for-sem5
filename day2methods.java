c3f9837c4128c27266e347a361bda62ec95f55cb
chandran-cell <2322jc54@kgcas.com>
2025-06-06 10:09:06 +0530

    Create day2methods.java

---
+++ day2methods.java
@@ -0,0 +1,24 @@
+package day2;
+import java.util.*;
+
+
+public class methods {
+	public static void Details(String name,String Job,String Package){
+
+System.out.println("hello,"+name+" "+Job+" "+Package);
+
+
+
+		
+	
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Details("ram","developer","4lpa");
+		Details("hari","trainer","4lpa");
+
+
+	}
+
+}
