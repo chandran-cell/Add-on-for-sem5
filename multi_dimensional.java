fa5afac49c34e9d298b80511aa48ba9af7943a89
chandran-cell <2322jc54@kgcas.com>
2025-06-06 14:44:21 +0530

    Create multi_dimensional.java

---
+++ multi_dimensional.java
@@ -0,0 +1,18 @@
+public class multi_dimensional {
+
+	public static void main(String[] args) {
+		
+
+		
+		int[][] number= {{1,2},{10,20}};
+		for(int i=0;i<2;i++) {
+			for(int j=0;j<2;j++) {
+				System.out.print(number[i][j]+" ");
+			}
+			System.out.println(" ");}
+			}
+		
+
+	}
+	
+
