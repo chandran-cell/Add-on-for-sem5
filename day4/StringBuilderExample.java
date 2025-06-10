0e38f4d5a4c3f451c21ca58ccbaa7132f7f94cc0
chandran-cell <2322jc54@kgcas.com>
2025-06-10 15:08:52 +0530

    Create StringBuilderExample.java

---
+++ day4/StringBuilderExample.java
@@ -0,0 +1,11 @@
+package day4;
+
+public class StringBuilderExample {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("hello world");
+        sb.reverse();
+        System.out.println(sb);
+    }
+
+}
