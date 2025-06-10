d85b8215fa58a8209d348dc52f2804adc943e4af
chandran-cell <2322jc54@kgcas.com>
2025-06-10 15:24:36 +0530

    Create StringBufferex.java

---
+++ day4/StringBufferex.java
@@ -0,0 +1,14 @@
+package day4;
+
+public class StringBufferex {
+
+	public static void main(String[] args) {
+		StringBuffer sb =new StringBuffer("Hello  java");
+		int a=sb.length();
+		System.out.println(a);
+		
+	}
+
+	
+
+}
