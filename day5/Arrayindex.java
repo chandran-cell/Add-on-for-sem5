17769c025e211e5e2ffb357523ea5ad6354b79c3
chandran-cell <2322jc54@kgcas.com>
2025-06-11 13:58:34 +0530

    Add files via upload

---
+++ day5/Arrayindex.java
@@ -0,0 +1,21 @@
+package program;
+
+public class main {
+
+	public static void main(String[] args) {
+		try{
+			int[] myNumber = {1,2,3};
+		
+		System.out.println(myNumber[10]);
+		}
+		catch( Exception e) {
+			System.out.println("The lenght is mismatch");
+			
+		}
+		finally {
+			System.out.println(" invald input");
+		}
+		
+	}
+
+}
