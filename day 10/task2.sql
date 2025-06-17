1c3e34f0838bea6309d49f7867fb5274d35d50e8
chandran-cell <2322jc54@kgcas.com>
2025-06-17 15:06:31 +0530

    Create task2.sql

---
+++ day 10/task2.sql
@@ -0,0 +1,13 @@
+mysql> SELECT * FROM Stdudent
+    -> ORDER BY fee ASC;
++--------+-------+------+------+---------+
+| rollno | sname | dep  | mark | fee     |
++--------+-------+------+------+---------+
+|      1 | soma  | bca  |  100 |    NULL |
+|      2 | soma  | bca  |   55 |    NULL |
+|      3 | ram   | bca  |   66 |    NULL |
+|      4 | raj   | bca  |   85 |    NULL |
+|      5 | rahul | bca  |   75 |    NULL |
+|      0 | NULL  | NULL | NULL | 5000000 |
++--------+-------+------+------+---------+
+6 rows in set (0.00 sec)
